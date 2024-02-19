a0a71e6e142cc19334c9ddd22ef9b184c114f53c
WafaHawbani <105780224+WafaHawbani@users.noreply.github.com>
2024-02-19 11:00:43 +0300

    Add files via upload

---
+++ mainf.js
@@ -0,0 +1,155 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve);
+  });
+};
+
+(function () {
+  const parseMetadata = metadata => {
+    const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+    const dimensions = []
+    for (const key in dimensionsMap) {
+      const dimension = dimensionsMap[key]
+      dimensions.push({ key, ...dimension })
+    }
+    const measures = []
+    for (const key in measuresMap) {
+      const measure = measuresMap[key]
+      measures.push({ key, ...measure })
+    }
+    return { dimensions, measures, dimensionsMap, measuresMap }
+  }
+
+  const appendTotal = (data) => { //there is total here
+    data = JSON.parse(JSON.stringify(data))
+    const superRoot = {
+      dimensions_0: { id: 'total', label: 'Total' },
+      measures_0: { raw: 0 }
+    }
+    data.forEach(data => {
+      if (data.dimensions_0.parentId) { return }
+      data.dimensions_0.parentId = 'total'
+      superRoot.measures_0.raw += data.measures_0.raw
+    })
+    return [superRoot].concat(data)
+  }
+
+  class Renderer {
+    constructor (root) {
+      this._root = root
+      this._echart = null
+    }
+
+    async render (dataBinding) {
+      await getScriptPromisify("https://dasarimadhusapbo.github.io/SankeyWidget/echarts.min.js");
+      this.dispose()
+
+      if (dataBinding.state !== 'success') { return }
+
+      let { data, metadata } = dataBinding
+      const { dimensions, measures } = parseMetadata(metadata)
+
+      const [dimension] = dimensions
+      const [measure] = measures
+      const nodes = []
+      const links = []
+
+      data = appendTotal(data)
+     data.forEach(row => {
+          // dimension
+          categoryData.push(dimensions.map(dimension => {
+            return row[dimension.key].label
+          }).join('/'))
+          // measures
+          series.forEach(series => {
+            series.data.push(row[series.key].raw)
+          })
+        })
+      
+      // https://echarts.apache.org/examples/en/editor.html?c=sankey-levels
+     const myChart = echarts.init(this._root, 'main')
+        const option = {
+          xAxis: {
+            type: 'category',
+            data: categoryData
+          },
+          yAxis: {
+            type: 'value'
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          series
+        }
+        myChart.setOption(option)
+    }
+
+    dispose () {
+      if (this._echart) {
+        echarts.dispose(this._echart)
+      }
+    }
+  }
+
+  const template = document.createElement('template')
+  template.innerHTML = `
+  <style>
+      #chart {
+          width: 100%;
+          height: 100%;
+      }
+  </style>
+  <div id="root" style="width: 100%; height: 100%;">
+      <div id="chart"></div>
+  </div>
+  `
+
+  class Main extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
+      this._root = this._shadowRoot.getElementById('root')
+      this._renderer = new Renderer(this._root)
+    }
+
+    // ------------------
+    // LifecycleCallbacks
+    // ------------------
+    async onCustomWidgetBeforeUpdate (changedProps) {
+    }
+
+    async onCustomWidgetAfterUpdate (changedProps) {
+      this.render()
+    }
+
+    async onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    async onCustomWidgetDestroy () {
+      this.dispose()
+    }
+
+    // ------------------
+    //
+    // ------------------
+    render () {
+      if (!document.contains(this)) {
+        // Delay the render to assure the custom widget is appended on dom
+        setTimeout(this.render.bind(this), 0)
+        return
+      }
+
+      this._renderer.render(this.dataBinding)
+    }
+
+    dispose () {
+      this._renderer.dispose()
+    }
+  }
+
+  customElements.define('com-sap-sac-sample-echarts-sankey', Main)
+})()
+
